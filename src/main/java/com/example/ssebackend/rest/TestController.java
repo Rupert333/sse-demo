255cf4b1e8a34899ce7b197d4a492a21dd6f6507
rupert <535427677@qq.com>
2025-07-08 16:05:02 +0800

    feat: 引入id生成器

--- src/main/java/com/example/ssebackend/rest/TestController.java
+++ src/main/java/com/example/ssebackend/rest/TestController.java
@@ -1,16 +1,28 @@
 package com.example.ssebackend.rest;
 
+import com.block.idgenerator.IdGenerator;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 @RequestMapping("/api/test")
 public class TestController {
+    @Resource
+    private IdGenerator idGenerator;
 
     // curl http://localhost:8080/api/test
     @GetMapping
     public String testEndpoint() {
         return "Hello, this is a test endpoint!";
     }
+
+    // curl http://localhost:8080/api/test/id
+    @GetMapping("/id")
+    public String testEndpointWithId() {
+        return idGenerator.generate();
+    }
+
 }
